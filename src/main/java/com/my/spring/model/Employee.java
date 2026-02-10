c580ae5813e308b100e2a7edf745014b09dbcaf1
sunnykumar2001 <sunnykumar18122001@gmail.com>
2026-02-10 14:33:21 +0530

    update

--- src/main/java/com/my/spring/model/Employee.java
+++ src/main/java/com/my/spring/model/Employee.java
@@ -5,6 +5,8 @@ public class Employee {
 	private int id;
 	private String name, gender;
 
+	private Address address;
+
 	public Employee() {
 		super();
 		System.out.println("Employee.Employee()");
@@ -12,13 +14,20 @@ public class Employee {
 		// TODO Auto-generated constructor stub
 	}
 
-	public Employee(int id, String name, String gender) {
+	public Employee(int id, String name, String gender, Address address) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.gender = gender;
-		System.out.println("Employee.Employee(3)");
+		this.address = address;
+	}
 
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
 	}
 
 	public int getId() {
@@ -50,7 +59,7 @@ public class Employee {
 
 	@Override
 	public String toString() {
-		return "Employee [id=" + id + ", name=" + name + ", gender=" + gender + "]";
+		return "Employee [id=" + id + ", name=" + name + ", gender=" + gender + ", address=" + address + "]";
 	}
 
 }
