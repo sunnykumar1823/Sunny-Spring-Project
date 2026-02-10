3b5a10d46f0cf52bf0b2d6bb7fa5b12dbdcb79cc
sunnykumar2001 <sunnykumar18122001@gmail.com>
2026-02-10 19:08:55 +0530

    update

--- src/main/java/com/my/spring/model/Employee.java
+++ src/main/java/com/my/spring/model/Employee.java
@@ -1,11 +1,22 @@
 package com.my.spring.model;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 public class Employee {
 
 	private int id;
+
 	private String name, gender;
 
 	private Address address;
+
+	private List<Integer> myList;
+
+	private Set<Integer> setOfContacts;
+
+	private Map<Integer, String> map;
 
 	public Employee() {
 		super();
@@ -14,12 +25,43 @@ public class Employee {
 		// TODO Auto-generated constructor stub
 	}
 
-	public Employee(int id, String name, String gender, Address address) {
+	public Employee(int id, String name, String gender, Address address, List<Integer> myList,
+			Set<Integer> setOfContacts, Map<Integer, String> map) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.gender = gender;
 		this.address = address;
+		this.myList = myList;
+		this.setOfContacts = setOfContacts;
+		this.map = map;
+	}
+
+	public List<Integer> getMyList() {
+		return myList;
+	}
+
+	public void setMyList(List<Integer> myList) {
+		this.myList = myList;
+		System.out.println("Employee.setMyList()");
+	}
+
+	public Set<Integer> getSetOfContacts() {
+		return setOfContacts;
+	}
+
+	public void setSetOfContacts(Set<Integer> setOfContacts) {
+		this.setOfContacts = setOfContacts;
+		System.out.println("Employee.setSetOfContacts()");
+	}
+
+	public Map<Integer, String> getMap() {
+		return map;
+	}
+
+	public void setMap(Map<Integer, String> map) {
+		this.map = map;
+		System.out.println("Employee.setMap()");
 	}
 
 	public Address getAddress() {
@@ -28,6 +70,7 @@ public class Employee {
 
 	public void setAddress(Address address) {
 		this.address = address;
+		System.out.println("Employee.setAddress()");
 	}
 
 	public int getId() {
@@ -35,8 +78,8 @@ public class Employee {
 	}
 
 	public void setId(int id) {
-		System.out.println("Employee.setId()");
 		this.id = id;
+		System.out.println("Employee.setId()");
 	}
 
 	public String getName() {
@@ -44,8 +87,9 @@ public class Employee {
 	}
 
 	public void setName(String name) {
-		System.out.println("Employee.setName()");
 		this.name = name;
+		System.out.println("Employee.setName()");
+
 	}
 
 	public String getGender() {
@@ -53,13 +97,15 @@ public class Employee {
 	}
 
 	public void setGender(String gender) {
-		System.out.println("Employee.setGender()");
 		this.gender = gender;
+		System.out.println("Employee.setGender()");
+
 	}
 
 	@Override
 	public String toString() {
-		return "Employee [id=" + id + ", name=" + name + ", gender=" + gender + ", address=" + address + "]";
+		return "Employee [id=" + id + ", name=" + name + ", gender=" + gender + ", address=" + address + ", myList="
+				+ myList + ", setOfContacts=" + setOfContacts + ", map=" + map + "]";
 	}
 
 }
